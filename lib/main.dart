c105750fe95565505dd6ad6026da0857ad95d356
halityrd <halityrd@hotmail.com>
2025-08-15 09:16:06 +0300

    Added Expense Data Model

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
+import 'package:expense_tracker/expenses.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold()));
+  runApp(const MaterialApp(home: Expenses()));
 }
